c352ed1918f615f8d74fb3ccbeeeccb28ed9fa42
ThatEpicBanana <thatepicbanana132@gmail.com>
2022-08-04 12:08:22 -0400

    added some apple-specific utilites

--- apple.sh
+++ apple.sh
@@ -36,6 +36,10 @@ brew install 7zip
 brew install mold
 # sudo apt install clang
 
+# apple-specific stuff
+brew install --cask background-music
+brew install iterm2
+
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -52,6 +56,7 @@ gsed -i -r "s/plugins\s*=\s*(\(\s*)((\w+ )*)(\w*\))/plugins=\1\2$plugins \4/g" ~
 cd $dir
 
 cp -r config/* ~/.config/
+cp -r apple/Preferences/* ~/Library/Preferences/
 
 cat home/.zshrc >> ~/.zshrc
 cat "alias 7z=7zz" >> ~/.zshrc
