3563077c41f6926631dc77e7d40e2e67823478e2
ThatEpicBanana <thatepicbanana132@gmail.com>
2022-09-12 01:19:43 -0400

    i forgot about neovide

--- common/neovim/config/nvim/lua/thate/init.lua
+++ common/neovim/config/nvim/lua/thate/init.lua
@@ -9,8 +9,9 @@ require("config.ui")
 require("config.utilites")
 require("config.visuals")
 
-if vim.fn.has('win32') then
+if vim.fn.has('win32') == 1 then
   vim.cmd('source ' .. vim.opt.runtimepath:get()[1] .. [[\lua\config\coc.vim]])
+  vim.cmd('source ' .. vim.opt.runtimepath:get()[1] .. [[\lua\config\neovide.vim]])
 else
-  vim.cmd('source ' .. vim.opt.runtimepath:get()[1] .. [[/lua/config/coc.vim]])
+  vim.cmd('source ' .. vim.opt.runtimepath:get()[1] .. [[/lua/config/neovide.vim]])
 end
