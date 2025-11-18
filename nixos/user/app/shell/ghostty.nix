946aa60176b4ead33118446f6bcdf34892c0d0f9
baanan <thatepicbanana132@gmail.com>
2025-11-18 15:09:14 -0500

    Tue Nov 18 03:09:14 PM EST 2025

---
+++ nixos/user/app/shell/ghostty.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ pkgs.ghostty ];
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "TokyoNight Moon";
+      keybind = [
+        "ctrl+q=close_surface";
+
+        "ctrl+v=new_split:right";
+        "ctrl+s=new_split:down";
+
+        "ctrl+alt+h=goto_split:left";
+        "ctrl+alt+j=goto_split:down";
+        "ctrl+alt+k=goto_split:up";
+        "ctrl+alt+l=goto_split:right";
+
+        "alt+shift+h=resize_split:left,1";
+        "alt+shift+j=resize_split:down,1";
+        "alt+shift+k=resize_split:up,1";
+        "alt+shift+l=resize_split:right,1";
+
+        "ctrl+shift+t=new_tab";
+        "ctrl+shift+h=previous_tab";
+        "ctrl+shift+l=next_tab";
+      ];
+    };
+  };
+}
