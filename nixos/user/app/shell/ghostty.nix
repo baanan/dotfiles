e1d8cd24502c275054c08c2925689608a399ec92
baanan <thatepicbanana132@gmail.com>
2025-11-19 10:24:19 -0500

    Wed Nov 19 10:24:19 AM EST 2025

--- nixos/user/app/shell/ghostty.nix
+++ nixos/user/app/shell/ghostty.nix
@@ -6,11 +6,12 @@
     enable = true;
     settings = {
       theme = "tokyonight_moon";
-      mouse-scroll-multiplier = 0.5;
-      background-opacity = 0.8;
+      mouse-scroll-multiplier = 0.1;
+      background-opacity = 0.9;
       maximize = true;
       # window-show-tab-bar = "never";
       gtk-tabs-location = "hidden";
+      # gtk-titlebar-style = "tabs";
       keybind = [
         "ctrl+q=close_surface"
 
@@ -22,10 +23,10 @@
         "ctrl+alt+k=goto_split:up"
         "ctrl+alt+l=goto_split:right"
 
-        "alt+shift+h=resize_split:left,1"
-        "alt+shift+j=resize_split:down,1"
-        "alt+shift+k=resize_split:up,1"
-        "alt+shift+l=resize_split:right,1"
+        "alt+shift+h=resize_split:left,10"
+        "alt+shift+j=resize_split:down,10"
+        "alt+shift+k=resize_split:up,10"
+        "alt+shift+l=resize_split:right,10"
 
         "ctrl+shift+t=new_tab"
         "ctrl+shift+h=previous_tab"
--- nixos/user/wm/gnome/gnome.nix
+++ nixos/user/wm/gnome/gnome.nix
@@ -5,4 +5,16 @@
     ./extensions.nix 
     ./theming.nix
   ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "vesktop.desktop"
+        # "kitty.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "org.gnome.Nautilus.desktop"
+      ];
+    };
+  };
 }
