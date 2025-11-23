d3c3e35bc5202330fd2f321bdda6c1388fcd4756
baanan <thatepicbanana132@gmail.com>
2025-11-23 11:30:24 -0500

    Sun Nov 23 11:30:24 AM EST 2025

--- nixos/profiles/work/configuration.nix
+++ nixos/profiles/work/configuration.nix
@@ -30,6 +30,7 @@
 
   # Set your time zone.
   time.timeZone = "America/New_York";
+  time.hardwareClockInLocalTime = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
