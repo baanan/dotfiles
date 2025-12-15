9227eaec4e622cccddc6fd8f966a7fc8aee07d0d
baanan <thatepicbanana132@gmail.com>
2025-12-15 00:32:52 -0500

    Mon Dec 15 12:32:52 AM EST 2025

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "nixpkgs/master";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     xremap-flake.url = "github:xremap/nix-flake";
