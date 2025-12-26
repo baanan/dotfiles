ead10c2e29cac25b6e3b9fc9262cf06a49f27ce1
baanan <thatepicbanana132@gmail.com>
2025-12-26 17:29:05 -0500

    Fri Dec 26 05:29:05 PM EST 2025

--- nixos/user/app/system/all.nix
+++ nixos/user/app/system/all.nix
@@ -1,9 +1,9 @@
-{ pkgs, pkgsUnstable, ... }:
+{ pkgs, pkgsUnstable, profile, ... }:
 
 {
   home.packages = with pkgs; [
     mission-center protonvpn-gui unzip
-  ];
+  ] ++ (if profile == "desktop" then [ openrgb ] else []);
 
   services.flatpak.packages = [
     "com.usebottles.bottles" "com.github.tchx84.Flatseal"
