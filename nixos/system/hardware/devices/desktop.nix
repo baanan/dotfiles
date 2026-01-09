f9b2905a281f5ecf26fa5324362a6ef327916c6b
baanan <thatepicbanana132@gmail.com>
2026-01-09 04:04:11 -0500

    Fri Jan  9 04:04:11 AM EST 2026

--- nixos/system/hardware/devices/desktop.nix
+++ nixos/system/hardware/devices/desktop.nix
@@ -35,6 +35,11 @@
       fsType = "ext4";
     };
 
+  fileSystems."/mnt/tera" =
+    { device = "/dev/disk/by-uuid/8832ECEB32ECDF66";
+      fsType = "ntfs";
+    };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
