663fa48ff20de529bf6a65e7c00755e97f0f6e69
baanan <thatepicbanana132@gmail.com>
2026-01-23 16:55:34 -0500

    Fri Jan 23 04:55:34 PM EST 2026

--- nixos/user/app/code/zed.nix
+++ nixos/user/app/code/zed.nix
@@ -3,12 +3,8 @@
 {
   home.packages = with pkgs; [ ];
 
+  home.file.".config/zed".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/projects/dotfiles/common/zed/config";
   programs.zed-editor = {
     enable = true;
-    extensions = [ "nix" "catppuccin" "catppuccin-icons" ];
-    userSettings = {
-      disable_ai = true;
-      vim_mode = false;
-    };
   };
 }
