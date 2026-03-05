fa5127de3221fe4d93f117ebbab0aa59199ffbbd
Brennan Craig <thatepicbanana132@gmail.com>
2026-03-05 10:20:54 -0500

    Thu Mar  5 10:20:54 AM EST 2026

--- nixos/user/app/code/all.nix
+++ nixos/user/app/code/all.nix
@@ -1,50 +1,75 @@
-{ pkgs, pkgsUnstable, profile, lib, ... }:
+{
+  pkgs,
+  pkgsUnstable,
+  profile,
+  lib,
+  ...
+}:
 
-lib.recursiveUpdate {
-  imports = [
-    ./java.nix
-    ./rust.nix
-    ./nvim.nix
-    ./lua.nix
-    ./c.nix
-    ./js.nix
-    ./ruby.nix
-    ./zed.nix
-    ./nix.nix
-    ./go.nix
-  ];
+lib.recursiveUpdate
+  {
+    imports = [
+      ./java.nix
+      ./rust.nix
+      ./nvim.nix
+      ./lua.nix
+      ./c.nix
+      ./js.nix
+      ./ruby.nix
+      ./zed.nix
+      ./nix.nix
+      ./go.nix
+    ];
 
-  programs = {
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      silent = true;
-      nix-direnv.enable = true;
-    };
+    programs = {
+      direnv = {
+        enable = true;
+        enableZshIntegration = true;
+        silent = true;
+        nix-direnv.enable = true;
+      };
 
-    git = {
-      enable = true;
-      userEmail = "thatepicbanana132@gmail.com";
-      userName = "baanan";
+      git = {
+        enable = true;
+        userEmail = "thatepicbanana132@gmail.com";
+        userName = "baanan";
 
-      extraConfig = {
-        safe = {
-          directory = "*";
+        extraConfig = {
+          safe = {
+            directory = "*";
+          };
         };
       };
     };
-  };
 
-  home.packages = with pkgs; [
-    tree-sitter gdb
-    # typst
-    typst typst-live tinymist
-  ] ++ (if profile != "macos" then [
-    thonny
-  ] else []);
-}
-(if profile != "macos" then {
-  services.flatpak.packages = [
-    "org.gnome.design.IconLibrary"
-  ];
-} else {})
+    home.packages =
+      with pkgs;
+      [
+        tree-sitter
+        gdb
+        # typst
+        typst
+        typst-live
+        tinymist
+        # haskell
+        haskell-language-server
+      ]
+      ++ (
+        if profile != "macos" then
+          [
+            thonny
+          ]
+        else
+          [ ]
+      );
+  }
+  (
+    if profile != "macos" then
+      {
+        services.flatpak.packages = [
+          "org.gnome.design.IconLibrary"
+        ];
+      }
+    else
+      { }
+  )
