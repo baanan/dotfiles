b0a1ff04b8068bcc5a38e59d02294934d7e42dd6
baanan <thatepicbanana132@gmail.com>
2026-05-28 11:04:27 -0400

    Thu May 28 11:04:27 AM EDT 2026

--- nixos/user/app/code/zed.nix
+++ nixos/user/app/code/zed.nix
@@ -1,10 +1,17 @@
-{ pkgs, config, system, profile, ... }:
+{
+  pkgs,
+  config,
+  pkgsUnstable,
+  ...
+}:
 
 {
   home.packages = with pkgs; [ ];
 
-  home.file.".config/zed".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/projects/dotfiles/common/zed/config";
+  home.file.".config/zed".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/projects/dotfiles/common/zed/config";
   programs.zed-editor = {
     enable = true;
+    package = pkgsUnstable.zed-editor;
   };
 }
