779cb3f1f7712c660a03146ac46092ebb30d1533
Brennan Craig <thatepicbanana132@gmail.com>
2026-06-25 16:17:25 -0400

    Thu Jun 25 04:17:25 PM EDT 2026

--- nixos/user/wm/gnome/extensions.nix
+++ nixos/user/wm/gnome/extensions.nix
@@ -4,7 +4,14 @@
   home.packages = [
     pkgs.gnomeExtensions.blur-my-shell
     pkgs.gnomeExtensions.dash-to-panel
-    pkgsUnstable.gnomeExtensions.pop-shell
+    (pkgs.gnomeExtensions.pop-shell.overrideAttrs (_: {
+      src = pkgs.fetchFromGitHub {
+        owner = "pop-os";
+        repo = "shell";
+        rev = "7898b65c20735057faf0797f8ed056704ca55f0d";
+        hash = "sha256-MmHoOxymo0QSRbRcSbFiv82+QWAwIwXwg/wyGQGVYiI=";
+      };
+    }))
     pkgs.gnomeExtensions.appindicator
   ];
   dconf.settings = {
